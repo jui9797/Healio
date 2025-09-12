2c65a856c2849d4680d262c3b323847863c1e46d
Lenovo <jannatjui414@gmail.com>
2025-09-12 20:01:06 +0600

    feat: fix dashboard route

---
+++ src/app/dashboard/page.jsx
@@ -0,0 +1,9 @@
+const Dashboard = () => {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <h3>this is dashboard</h3>
+    </div>
+  );
+};
+
+export default Dashboard;
